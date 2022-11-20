d9a6d8beb73d1b0614e6c643697418590b73e92a
Kitpivat Piromgit <103819686+shirocola@users.noreply.github.com>
2022-11-20 19:38:29 +0700

    add login

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo  from '../../assets/react.svg'
 import './App.css'
 import HelloWorld from '../HelloWorld'
+import Login from '../Login/Login'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -25,6 +28,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <Login/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
