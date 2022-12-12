efe84a1ef9dc38247f19fd4c469c2e5d91317035
Patipan.m <114389220+flooksan@users.noreply.github.com>
2022-12-12 14:47:39 +0700

    Features: Dashboard is homepage , AddActivity for add user activity and EditActivity for user edit there card.

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,12 @@
+import React,{ useState, useEffect} from "react";
 import './Dashboard.css'
 import profilePicture from '../../assets/man.png'
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import MainCard from "../Card/MainCard";
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 export const data = { 
@@ -37,44 +42,103 @@ const link = () => {
 }
 
 const Dashboard = () => {
+    
+    const [card,setCard] =useState([
+        {_id:'222',activity:'run',decripttion:'gogogo'},
+        {_id:'222',activity:'run',decripttion:'gogogo'},
+        {_id:'222',activity:'run',decripttion:'gogogo'},
+    ]);
+    
+   
+
+    // const {_id,activity,decripttion,endDate,startDate} = card;
+    
+    
+    
     return(
-        <div className="container-summary">
-            <div className="display-card">
-                <span>Hello, </span>
-                <h2>Display Name</h2>
-                <img src={profilePicture} alt="profile-picture" className="profile-picture" onClick={link}/>
-                
-                <div className="graph">
-                    <Doughnut data={data} />
-                </div>
-                
-                <div className="activity-container">
-                    <div className="activity">
-                        <p>Total activity:</p>
-                        <span className="gray">|</span>
-                        <span>99</span>                                         
-                    </div>
-                    <div className="activity">
-                        <p>Completed:</p>
-                        <span className="green">|</span>
-                        <span>20</span>         
-                    </div>
+        
+        
+
+
+    <div className="container">
+            
+            <div className="left">
+            Container
+
+                <div className='left-top'>
+                    <a href="/addactivity">AddActivity</a>
                 </div>
 
-                <div className="activity-container">
-                     <div className="activity">
-                        <p>In progress:</p>
-                        <span className="yellow">|</span>
-                        <span>71</span>         
+                <div className='left-bottom'>
+                    Bottom
+                    <div className="grid-container">
+                        <Grid container 
+                        direction="row"
+                        justifyContent="center" 
+                        alignItems="center"
+                        spacing={{ xs: 2, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }} >
+                            
+                            {card.length >0 && card.map((carditem,index) => 
+                                <Grid item xs={2} sm={4} md={4} key={index}>
+                                    <MainCard key={carditem._id} id={carditem._id}/>
+                                </Grid>
+                            )}
+                            
+                            {/* {Array.from(Array(6)).map((_, index) => (
+                                <Grid item xs={2} sm={4} md={4} key={index}>
+                                    <SwimCard props={card} />
+                                </Grid>
+                            ))} */}
+
+                        </Grid>
                     </div>
-                    <div className="activity">
-                        <p>Incomplete:</p>
-                        <span className="red">|</span>
-                        <span>8</span>         
-                    </div>  
+                    
+                    
                 </div>
-               
-            </div>        
+
+            </div>
+            <div className="right">
+            
+                    <div className="container-summary">
+                        <div className="display-card">
+                            <span>Hello, </span>
+                            <h2>Display Name</h2>
+                            <img src={profilePicture} alt="profile-picture" className="profile-picture" onClick={link} />
+
+                            <div className="graph">
+                                <Doughnut data={data} />
+                            </div>
+
+                            <div className="activity-container">
+                                <div className="activity">
+                                    <p>Total activity:</p>
+                                    <span className="gray">|</span>
+                                    <span>99</span>
+                                </div>
+                                <div className="activity">
+                                    <p>Completed:</p>
+                                    <span className="green">|</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+
+                            <div className="activity-container">
+                                <div className="activity">
+                                    <p>In progress:</p>
+                                    <span className="yellow">|</span>
+                                    <span>71</span>
+                                </div>
+                                <div className="activity">
+                                    <p>Incomplete:</p>
+                                    <span className="red">|</span>
+                                    <span>8</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+            </div>
+        
         </div>
     )
 }
