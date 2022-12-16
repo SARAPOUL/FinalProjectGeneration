e5a77303f5c8e83f3e3cdf9aefd1f76c49a593e2
sarapoul <pormee2001@gmail.com>
2022-12-16 21:10:43 +0700

    fix:add/edit

---
+++ src/Components/Activity/Activity.jsx
@@ -0,0 +1,213 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import TextField from '@mui/material/TextField';
+import { useNavigate } from 'react-router-dom';
+import { InputLabel, Select, MenuItem } from '@mui/material';
+import './Activity.css'
+import Grid from '@mui/material/Grid'; // Grid version 1';
+
+const Activity = (props) => {
+    // State of activity
+    const [state, setState] = useState({
+        _id: props.activity ? props.activity._id : "",
+        userId: props.activity ? props.activity.userId : "",
+        activityName: props.activity ? props.activity.activityName : "",
+        activityType: props.activity ? props.activity.activityType : "",
+        startDate: props.activity ? props.activity.startDate : "",
+        endDate: props.activity ? props.activity.endDate : "",
+        duration: props.activity ? props.activity.duration : "",
+        decripttion: props.activity ? props.activity.decripttion : "",
+    })
+    const { _id, userId, activityName, activityType, startDate, endDate, duration, decripttion } = state
+    const navigate = useNavigate();
+    // Function
+
+    const setActivityName = (e) => {
+        setState({ ...state, activityName: e.target.value })
+    }
+    const selectActivity = (e) => {
+        setState({ ...state, activityType: e.target.value })
+    }
+    const setDescriptiton = (e) => {
+        setState({ ...state, decripttion: e.target.value })
+    }
+    const selectstartdate = (e) => {
+        setState({ ...state, startDate: e.target.value })
+    }
+
+    const selectenddate = (e) => {
+        setState({ ...state, endDate: e.target.value })
+    }
+
+    const setDuration = (e) => {
+        setState({ ...state, duration: e.target.value })
+    }
+
+
+    function getDateDifference() {
+        const tempStartDate = new Date(startDate);
+        const tempEndDate = new Date(endDate);
+        const timeDifference = Math.abs(tempEndDate - tempStartDate);
+        const diffHours = Math.floor(timeDifference / (1000 * 3600));
+        const diffMinutes = Math.floor((timeDifference % (1000 * 3600)) / (1000 * 60));
+        const diffSeconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const formattedTimeDifference = `${diffHours.toString().padStart(2, '0')}:${diffMinutes.toString().padStart(2, '0')}:${diffSeconds.toString().padStart(2, '0')}`;
+        return formattedTimeDifference;
+    }
+
+    useEffect(() => {
+        setState({ ...state, duration: getDateDifference() })
+        // This function will be called whenever the value of date1 or date2 changes
+        // console.log('The difference between the selected dates has changed');
+    }, [startDate, endDate]);
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        if(props.activity){
+            //edit 
+        }else{
+            //add
+        }
+        console.log(state)
+        // setState({
+        //     activity:"",
+        //     startDate:"",
+        //     endDate:"",
+        //     decripttion:"",
+        // })
+    }
+
+    const onBackClick = useCallback(() => {
+        navigate('../dashboard')
+    }, [navigate])
+    const minDate = new Date().toISOString().substr(0, 16);
+
+
+    return (
+        <div className='form-activity' style={{ margin: "32px" }}>
+
+            <form className='form-add' onSubmit={submitForm}>
+                <Grid container spacing={0} margin={2}>
+                    <Grid item xs={12} md={2}>
+                        <label className=''>Activity Name: </label>
+                    </Grid>
+                    <Grid item xs={12} md={10}>
+                        <TextField id="activity_name" label="Activity Name" variant="outlined" value={activityName} validators={["required"]} onChange={setActivityName} required />
+                        {/* <input type="text" className='form-control' onChange={setActivityName} /> */}
+                    </Grid>
+                </Grid>
+                <Grid container spacing={0} margin={2}>
+                    <Grid item xs={12} md={2}>
+                        <label id="activity_type">Activity Type: </label>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={activityType}
+                            label="Activity Type"
+                            onChange={selectActivity}
+                        >
+                            <MenuItem value="walk">Walk</MenuItem>
+                            <MenuItem value="run">Run</MenuItem>
+                            <MenuItem value="hiking">Hiking</MenuItem>
+                            <MenuItem value="swim">Swim</MenuItem>
+                            <MenuItem value="bike">Bike</MenuItem>
+                        </Select>
+                        {/* <label className=''>Select Activity: </label>
+                    <select className='form-select mb-3' onChange={selectActivity}>
+                        <option value="walk">Walk</option>
+                        <option value="run">Run</option>
+                        <option value="hiking">Hiking</option>
+                        <option value="swim">Swim</option>
+                        <option value="bike">Bike</option>
+                    </select> */}
+                    </Grid>
+                </Grid>
+                {/* date start */}
+                <Grid container spacing={0} margin={2}>
+                    <Grid item xs={12} md={2}>
+                        <label className=''>Start : </label>
+                    </Grid>
+                    <Grid item xs={12} md={10}>
+                        <TextField className=''
+                            id="start_date"
+                            label="Start :"
+                            type="datetime-local"
+                            value={startDate}
+                            min={minDate}
+                            // defaultValue="2022-12-07T09:30"
+                            onChange={selectstartdate}
+                            sx={{ width: 250 }}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            required
+                        />
+                    </Grid>
+                </Grid>
+                {/* date end */}
+                <Grid container spacing={0} margin={2}>
+                    <Grid item xs={12} md={2}>
+                        <label className=''>End : </label>
+                    </Grid>
+                    <Grid item xs={12} md={10}>
+                        <TextField className='m-3'
+                            id="end_date"
+                            label="End :"
+                            type="datetime-local"
+                            value={endDate}
+                            // defaultValue="2022-12-28T10:30"
+                            min={startDate}
+                            onChange={selectenddate}
+                            sx={{ width: 250 }}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            required
+                        />
+                    </Grid>
+                </Grid>
+
+                <Grid container spacing={0} margin={2}>
+                    <Grid item xs={12} md={2}>
+                        <label className=''>Duration : </label>
+                    </Grid>
+                    <Grid item xs={12} md={10}>
+                        <TextField id="duration" label="" variant="outlined" value={duration} onChange={setDuration} disabled />
+                    </Grid>
+                </Grid>
+
+                <Grid container spacing={0} margin={2}>
+                    <Grid item xs={12} md={2}>
+                        <label className=''>Description: </label>
+                    </Grid>
+                    <Grid item xs={12} md={10}>
+                        <TextField
+                            sx={{ borderRadius: '40%' }}
+                            id="Decripttion"
+                            aria-label="minimum height"
+                            minRows={3}
+                            placeholder="Minimum 3 rows"
+                            multiline
+                            label="Decripttion"
+                            onChange={setDescriptiton}
+                            name="Decripttion"
+                            value={decripttion}
+                        // validators={["required"]}
+                        // errorMessages={["this field is required"]}
+                        />
+                    </Grid>
+                </Grid>
+
+
+                {/* Button */}
+                <button type="cancle" onClick={onBackClick} className="" style={{ backgroundColor: "#C32B42", marginRight: "16px" }}>Cancle</button>
+                <button type="submit" value="submit" className="">Save</button>
+
+
+            </form>
+        </div>
+    )
+}
+
+export default Activity
