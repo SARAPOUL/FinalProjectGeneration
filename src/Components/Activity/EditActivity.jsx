1f06cf30b8fc390df584fde7f5a1e5c0f8169e8a
sarapoul <pormee2001@gmail.com>
2022-12-22 15:02:43 +0700

    fix:png

--- src/Components/Activity/EditActivity.jsx
+++ src/Components/Activity/EditActivity.jsx
@@ -8,7 +8,7 @@ import editheader from '../../assets/edit.png'
 import axios from 'axios';
 
 //activityPicture
-import runimg from '../../assets/activityPicture/editrun.PNG'
+import runimg from '../../assets/activityPicture/editrun.png'
 import bikeimg from '../../assets/activityPicture/bike2.jpg'
 import walkimg from '../../assets/activityPicture/walk.jpg'
 import hikingimg from '../../assets/activityPicture/hiking2.jpg'
